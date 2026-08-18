41feaccfe7678388ddd8e5d5908ae88db86cc221
nagisa <2964793117@qq.com>
2026-08-18 20:46:10 +0800

    11

---
+++ train081801/source/stackforce_simready_train081801_gym/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="stackforce_simready_train081801_gym",
+    version="0.1.0",
+    description="StackForce SimReady Legged Gym export for train081801",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+)
